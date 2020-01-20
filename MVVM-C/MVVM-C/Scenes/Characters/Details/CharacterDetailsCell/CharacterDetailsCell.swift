bc48b0bfecd487b0f1784f0292626cbc41c94160
Dmytro Dobrovolskyy <dmytro.dobrovolskyy@cygility.com>
2020-01-20 17:18:05 +0200

    Added vertical/horizontal support in detailed view.

--- MVVM-C/MVVM-C/Scenes/Characters/Details/CharacterDetailsCell/CharacterDetailsCell.swift
+++ MVVM-C/MVVM-C/Scenes/Characters/Details/CharacterDetailsCell/CharacterDetailsCell.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 final class CharacterDetailsCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    var regularConstraints: [NSLayoutConstraint] = []
+    var compactConstraints: [NSLayoutConstraint] = []
 
     // MARK: - Inits
     
@@ -35,13 +40,6 @@ final class CharacterDetailsCell: UITableViewCell {
         
         addSubview(label)
         
-        NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 24),
-            label.bottomAnchor.constraint(equalTo: characterPhotoView.topAnchor, constant: -24),
-            label.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            label.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
-        ])
-        
         return label
     }()
     
@@ -55,13 +53,6 @@ final class CharacterDetailsCell: UITableViewCell {
         
         addSubview(imageView)
         
-        NSLayoutConstraint.activate([
-            imageView.bottomAnchor.constraint(equalTo: statusStackView.topAnchor, constant: -16),
-            imageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 48),
-            imageView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -48),
-            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1.0)
-        ])
-        
         return imageView
     }()
     
@@ -71,12 +62,6 @@ final class CharacterDetailsCell: UITableViewCell {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
         addSubview(stackView)
-        
-        NSLayoutConstraint.activate([
-            stackView.bottomAnchor.constraint(equalTo: speciesStackView.topAnchor, constant: -24),
-            stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
-        ])
         
         return stackView
     }()
@@ -88,12 +73,6 @@ final class CharacterDetailsCell: UITableViewCell {
         
         addSubview(stackView)
         
-        NSLayoutConstraint.activate([
-            stackView.bottomAnchor.constraint(equalTo: genderStackView.topAnchor, constant: -24),
-            stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
-        ])
-        
         return stackView
     }()
     
@@ -104,12 +83,6 @@ final class CharacterDetailsCell: UITableViewCell {
         
         addSubview(stackView)
         
-        NSLayoutConstraint.activate([
-            stackView.bottomAnchor.constraint(equalTo: currentLocationStackView.topAnchor, constant: -24),
-            stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
-        ])
-        
         return stackView
     }()
     
@@ -119,12 +92,6 @@ final class CharacterDetailsCell: UITableViewCell {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
         addSubview(stackView)
-        
-        NSLayoutConstraint.activate([
-            stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -24),
-            stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
-        ])
         
         return stackView
     }()
@@ -139,10 +106,116 @@ final class CharacterDetailsCell: UITableViewCell {
         speciesStackView.configure(withDescription: "Species", and: viewModel.species)
         genderStackView.configure(withDescription: "Gender", and: viewModel.gender)
         currentLocationStackView.configure(withDescription: "Current location", and: viewModel.currentLocation)
+        
+        configureConstraints()
+        activateCurrentConstraints()
+    }
+    
+    private func configureConstraints() {
+        // Name label constraints
+        regularConstraints.append(contentsOf: [
+            nameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 24),
+            nameLabel.bottomAnchor.constraint(equalTo: characterPhotoView.topAnchor, constant: -24),
+            nameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        compactConstraints.append(contentsOf: [
+            nameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 24),
+            nameLabel.leadingAnchor.constraint(equalTo: characterPhotoView.trailingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        // Photo view constraints
+        regularConstraints.append(contentsOf: [
+            characterPhotoView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            characterPhotoView.bottomAnchor.constraint(equalTo: statusStackView.topAnchor, constant: -16),
+            characterPhotoView.heightAnchor.constraint(equalTo: characterPhotoView.widthAnchor, multiplier: 1.0),
+            characterPhotoView.heightAnchor.constraint(equalToConstant: min(UIScreen.main.bounds.width, UIScreen.main.bounds.height) - 48)
+        ])
+        
+        compactConstraints.append(contentsOf: [
+            characterPhotoView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 24),
+            characterPhotoView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -24),
+            characterPhotoView.heightAnchor.constraint(equalTo: characterPhotoView.widthAnchor, multiplier: 1.0),
+            characterPhotoView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            characterPhotoView.heightAnchor.constraint(equalToConstant: min(UIScreen.main.bounds.width, UIScreen.main.bounds.height) - 48)
+        ])
+        
+        // Status stack view constraints
+        regularConstraints.append(contentsOf: [
+            statusStackView.bottomAnchor.constraint(equalTo: speciesStackView.topAnchor, constant: -24),
+            statusStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            statusStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        compactConstraints.append(contentsOf: [
+            statusStackView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 36),
+            statusStackView.leadingAnchor.constraint(equalTo: characterPhotoView.trailingAnchor, constant: 16),
+            statusStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        // Species stack view constraints
+        regularConstraints.append(contentsOf: [
+            speciesStackView.bottomAnchor.constraint(equalTo: genderStackView.topAnchor, constant: -24),
+            speciesStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            speciesStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        compactConstraints.append(contentsOf: [
+            speciesStackView.topAnchor.constraint(equalTo: statusStackView.bottomAnchor, constant: 24),
+            speciesStackView.leadingAnchor.constraint(equalTo: characterPhotoView.trailingAnchor, constant: 16),
+            speciesStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        // Gender stack view constraints
+        regularConstraints.append(contentsOf: [
+            genderStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            genderStackView.bottomAnchor.constraint(equalTo: currentLocationStackView.topAnchor, constant: -24),
+            genderStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        compactConstraints.append(contentsOf: [
+            genderStackView.topAnchor.constraint(equalTo: speciesStackView.bottomAnchor, constant: 24),
+            genderStackView.leadingAnchor.constraint(equalTo: characterPhotoView.trailingAnchor, constant: 16),
+            genderStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        // Current location stack view constraints
+        regularConstraints.append(contentsOf: [
+            currentLocationStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -24),
+            currentLocationStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            currentLocationStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        compactConstraints.append(contentsOf: [
+            currentLocationStackView.topAnchor.constraint(equalTo: genderStackView.bottomAnchor, constant: 24),
+            currentLocationStackView.leadingAnchor.constraint(equalTo: characterPhotoView.trailingAnchor, constant: 16),
+            currentLocationStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func activateCurrentConstraints() {
+        NSLayoutConstraint.deactivate(self.compactConstraints + self.regularConstraints)
+
+        if self.traitCollection.verticalSizeClass == .regular {
+            NSLayoutConstraint.activate(self.regularConstraints)
+        }
+        else {
+            NSLayoutConstraint.activate(self.compactConstraints)
+        }
     }
     
     private func initialConfigure() {
         backgroundColor = #colorLiteral(red: 0.9607843137, green: 0.9843137255, blue: 0.937254902, alpha: 1)
+    }
+
+    // MARK: - trait collections
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        self.activateCurrentConstraints()
     }
     
 }
